2dd6f041869b2b6b10cd00740c36fb22cce2227c
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-06-16 00:59:55 -0300

    fix: update allowed http method from put to patch

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   app.use(cors());
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE');
   res.header('Access-Control-Allow-Headers', '*');
   next();
 });
