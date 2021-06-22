10a146052a15cca0e19ad5f33daf5bf9549bec80
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-06-22 20:42:54 -0300

    refactor: change some config for hekoku deploy

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 const app = require('./index');
 const database = require('./config/database');
 
-const PORT = process.env.SERVER_PORT || 3333;
-
 database();
 
-app.listen(PORT, () => {
-  console.log(`[server] running on port:${PORT}`);
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`[server] running on heroku`);
 });
