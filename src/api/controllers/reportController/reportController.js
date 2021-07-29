e2d7048c4bb745337037149b7b09708ab00fa690
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-07-29 14:50:36 -0300

    feat: changes on reports

--- src/api/controllers/reportController/reportController.js
+++ src/api/controllers/reportController/reportController.js
@@ -8,50 +8,55 @@ exports.getReport = async (req, res) => {
   try {
     /* Patients */
 
-    const numberOfPatients = await Patient.countDocuments({});
+    const patients = await Patient.find({});
 
-    const numberOfFemalePatients = await Patient.countDocuments({
-      sex: 'Feminino',
-    });
+    const filteredFemalePatients = patients.filter(
+      ({ sex }) => sex === 'Feminino'
+    );
 
-    const numberOfMalePatients = await Patient.countDocuments({
-      sex: 'Masculino',
-    });
+    const filteredMalePatients = patients.filter(
+      ({ sex }) => sex === 'Masculino'
+    );
 
-    const numberOfIntersexPatients = await Patient.countDocuments({
-      sex: 'Intersexo',
-    });
+    const filteredIntersexPatients = patients.filter(
+      ({ sex }) => sex === 'Intersexo'
+    );
 
-    const acquiredSyphilisOcurrences = await Patient.countDocuments({
-      monitoringType: 'Sífilis adquirida',
-    });
+    const filteredAcquiredSyphilisOcurrences = patients.filter(
+      ({ monitoringType }) => monitoringType === 'Sífilis adquirida'
+    );
 
-    const congenitalSyphilisOcurrences = await Patient.countDocuments({
-      monitoringType: 'Sífilis congênita',
-    });
+    const filteredCongenitalSyphilisOcurrences = patients.filter(
+      ({ monitoringType }) => monitoringType === 'Sífilis congênita'
+    );
 
-    const gestationalSyphilisOcurrences = await Patient.countDocuments({
-      monitoringType: 'Sífilis em gestante',
-    });
+    const filteredGestationalSyphilisOcurrences = patients.filter(
+      ({ monitoringType }) => monitoringType === 'Sífilis em gestante'
+    );
 
-    // /* Exames */
+    /* Exames */
 
-    const numberOfExams = await Exam.countDocuments({});
-    const numberOfQuickTests = await Exam.countDocuments({
-      trepTestType: 'Teste rápido',
-    });
-    const numberOfIgMTests = await Exam.countDocuments({
-      trepTestType: 'FTA-ABS IgM',
-    });
-    const numberOfIgGTests = await Exam.countDocuments({
-      trepTestType: 'FTA-ABS IgG',
-    });
-    const numberOfReagentResults = await Exam.countDocuments({
-      trepTestResult: 'Reagente',
-    });
-    const numberOfNonReagentResults = await Exam.countDocuments({
-      trepTestResult: 'Não-reagente',
-    });
+    const exams = await Exam.find({});
+
+    const filteredQuickTests = exams.filter(
+      ({ trepTestType }) => trepTestType === 'Teste rápido'
+    );
+
+    const filteredIgMTests = exams.filter(
+      ({ trepTestType }) => trepTestType === 'FTA-ABS IgM'
+    );
+
+    const filteredIgGTests = exams.filter(
+      ({ trepTestType }) => trepTestType === 'FTA-ABS IgG'
+    );
+
+    const filteredReagentResults = exams.filter(
+      ({ trepTestResult }) => trepTestResult === 'Reagente'
+    );
+
+    const filteredNonReagentResults = exams.filter(
+      ({ trepTestResult }) => trepTestResult === 'Não-reagente'
+    );
 
     /* Notificações */
 
@@ -61,42 +66,49 @@ exports.getReport = async (req, res) => {
 
     const numberOfObservations = await Observation.countDocuments({});
 
-    // /* Tratamentos */
+    /* Tratamentos */
 
-    const numberOfTreatments = await Treatment.countDocuments({});
-    const numberOfTreatmentsUsingPenilicillin = await Treatment.countDocuments({
-      medication: 'Penicilina',
-    });
-    const numberOfTreatmentsUsingDoxycycline = await Treatment.countDocuments({
-      medication: 'Doxiciclina',
-    });
-    const numberOfTreatmentsUsingCeftriaxone = await Treatment.countDocuments({
-      medication: 'Ceftriaxona',
-    });
+    const treatments = await Treatment.find({});
+
+    const filteredTreatmentsUsingPenilillin = treatments.filter(
+      ({ medication }) => medication === 'Penicilina'
+    );
+
+    const filteredTreatmentsUsingDoxycycline = treatments.filter(
+      ({ medication }) => medication === 'Doxiciclina'
+    );
+
+    const filteredTreatmentsUsingCeftriaxone = treatments.filter(
+      ({ medication }) => medication === 'Ceftriaxona'
+    );
 
     res.status(200).json({
       status: 'success',
-      message: 'On development...',
       stats: {
-        numberOfPatients,
-        numberOfFemalePatients,
-        numberOfMalePatients,
-        numberOfIntersexPatients,
-        acquiredSyphilisOcurrences,
-        congenitalSyphilisOcurrences,
-        gestationalSyphilisOcurrences,
-        numberOfExams,
-        numberOfQuickTests,
-        numberOfIgMTests,
-        numberOfIgGTests,
-        numberOfReagentResults,
-        numberOfNonReagentResults,
+        numberOfPatients: patients.length,
+        numberOfFemalePatient: filteredFemalePatients.length,
+        numberOfMalePatients: filteredMalePatients.length,
+        numberOfIntersexPatients: filteredIntersexPatients.length,
+        acquiredSyphilisOcurrences: filteredAcquiredSyphilisOcurrences.length,
+        congenitalSyphilisOcurrences:
+          filteredCongenitalSyphilisOcurrences.length,
+        gestationalSyphilisOcurrences:
+          filteredGestationalSyphilisOcurrences.length,
+        numberOfExams: exams.length,
+        numberOfQuickTests: filteredQuickTests.length,
+        numberOfIgMTests: filteredIgMTests.length,
+        numberOfIgGTests: filteredIgGTests.length,
+        numberOfReagentResults: filteredReagentResults.length,
+        numberOfNonReagentResults: filteredNonReagentResults.length,
         numberOfNotifications,
         numberOfObservations,
-        numberOfTreatments,
-        numberOfTreatmentsUsingPenilicillin,
-        numberOfTreatmentsUsingDoxycycline,
-        numberOfTreatmentsUsingCeftriaxone,
+        numberOfTreatments: treatments.length,
+        numberOfTreatmentsUsingPenilicillin:
+          filteredTreatmentsUsingPenilillin.length,
+        numberOfTreatmentsUsingDoxycycline:
+          filteredTreatmentsUsingDoxycycline.length,
+        numberOfTreatmentsUsingCeftriaxone:
+          filteredTreatmentsUsingCeftriaxone.length,
       },
     });
   } catch (error) {
@@ -107,11 +119,134 @@ exports.getReport = async (req, res) => {
   }
 };
 
-/* exports.getReportByYear = async (req, res) => {
+exports.getReportByYear = async (req, res) => {
   try {
+    const { year } = req.params;
+
+    /* Pacientes */
+
+    const patients = await Patient.find({});
+
+    const filteredPatients = patients.filter(
+      ({ createdAt }) => createdAt.getFullYear() === +year
+    );
+
+    const filteredFemalePatients = filteredPatients.filter(
+      ({ sex }) => sex === 'Feminino'
+    );
+
+    const filteredMalePatients = filteredPatients.filter(
+      ({ sex }) => sex === 'Masculino'
+    );
+
+    const filteredIntersexPatients = filteredPatients.filter(
+      ({ sex }) => sex === 'Intersexo'
+    );
+
+    const filteredAcquiredSyphilisOcurrences = filteredPatients.filter(
+      ({ monitoringType }) => monitoringType === 'Sífilis adquirida'
+    );
+
+    const filteredCongenitalSyphilisOcurrences = filteredPatients.filter(
+      ({ monitoringType }) => monitoringType === 'Sífilis congênita'
+    );
+
+    const filteredGestationalSyphilisOcurrences = filteredPatients.filter(
+      ({ monitoringType }) => monitoringType === 'Sífilis em gestante'
+    );
+
+    /* Exames */
+
+    const exams = await Exam.find({});
+
+    const filteredExams = exams.filter(
+      ({ createdAt }) => createdAt.getFullYear() === +year
+    );
+
+    const filteredQuickTests = filteredExams.filter(
+      ({ trepTestType }) => trepTestType === 'Teste rápido'
+    );
+
+    const filteredIgMTests = filteredExams.filter(
+      ({ trepTestType }) => trepTestType === 'FTA-ABS IgM'
+    );
+
+    const filteredIgGTests = filteredExams.filter(
+      ({ trepTestType }) => trepTestType === 'FTA-ABS IgG'
+    );
+
+    const filteredReagentResults = filteredExams.filter(
+      ({ trepTestResult }) => trepTestResult === 'Reagente'
+    );
+
+    const filteredNonReagentResults = filteredExams.filter(
+      ({ trepTestResult }) => trepTestResult === 'Não-reagente'
+    );
+
+    /* Notificações */
+
+    const notifications = await Notification.find({});
+
+    const filteredNotifications = notifications.filter(
+      ({ createdAt }) => createdAt.getFullYear() === +year
+    );
+
+    /* Observações */
+
+    const observations = await Observation.find({});
+
+    const filteredObservations = observations.filter(
+      ({ createdAt }) => createdAt.getFullYear() === +year
+    );
+
+    /* Tratamentos */
+
+    const treatments = await Treatment.find({});
+
+    const filteredTreatments = treatments.filter(
+      ({ createdAt }) => createdAt.getFullYear() === +year
+    );
+
+    const filteredTreatmentsUsingPenilillin = filteredTreatments.filter(
+      ({ medication }) => medication === 'Penicilina'
+    );
+
+    const filteredTreatmentsUsingDoxycycline = filteredTreatments.filter(
+      ({ medication }) => medication === 'Doxiciclina'
+    );
+
+    const filteredTreatmentsUsingCeftriaxone = filteredTreatments.filter(
+      ({ medication }) => medication === 'Ceftriaxona'
+    );
+
     res.status(200).json({
       status: 'success',
-      message: 'On development...',
+      stats: {
+        numberOfPatients: filteredPatients.length,
+        numberOfFemalePatient: filteredFemalePatients.length,
+        numberOfMalePatients: filteredMalePatients.length,
+        numberOfIntersexPatients: filteredIntersexPatients.length,
+        acquiredSyphilisOcurrences: filteredAcquiredSyphilisOcurrences.length,
+        congenitalSyphilisOcurrences:
+          filteredCongenitalSyphilisOcurrences.length,
+        gestationalSyphilisOcurrences:
+          filteredGestationalSyphilisOcurrences.length,
+        numberOfExams: filteredExams.length,
+        numberOfQuickTests: filteredQuickTests.length,
+        numberOfIgMTests: filteredIgMTests.length,
+        numberOfIgGTests: filteredIgGTests.length,
+        numberOfReagentResults: filteredReagentResults.length,
+        numberOfNonReagentResults: filteredNonReagentResults.length,
+        numberOfNotifications: filteredNotifications.length,
+        numberOfObservations: filteredObservations.length,
+        numberOfTreatments: filteredTreatments.length,
+        numberOfTreatmentsUsingPenilicillin:
+          filteredTreatmentsUsingPenilillin.length,
+        numberOfTreatmentsUsingDoxycycline:
+          filteredTreatmentsUsingDoxycycline.length,
+        numberOfTreatmentsUsingCeftriaxone:
+          filteredTreatmentsUsingCeftriaxone.length,
+      },
     });
   } catch (error) {
     res.status(400).json({
@@ -120,4 +255,3 @@ exports.getReport = async (req, res) => {
     });
   }
 };
- */
--- src/api/routes/reportRoutes/reportRoutes.js
+++ src/api/routes/reportRoutes/reportRoutes.js
@@ -4,6 +4,6 @@ const { reportController } = require('../../controllers');
 const router = express.Router();
 
 router.route('/').get(reportController.getReport);
-// router.route('/:year').get(reportController.getReportByYear);
+router.route('/:year').get(reportController.getReportByYear);
 
 module.exports = router;
